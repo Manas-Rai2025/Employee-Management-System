91ad8632f548d15dd6fb75864aab580619d77f15
Manas-Rai2025 <manasrai95190@gmail.com>
2025-06-09 16:58:44 +0530

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,15 @@
+package com.yourname.employeemanager;
+
+public class Employee {
+    public int id;
+    public String name;
+    public String department;
+    public int salary;
+
+    public Employee(int id, String name, String department, int salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+}
